35185401a5dca863e951b4424da4b9bbd707379d
eschlicter <emilyschlicter@Emilys-iMac.local>
2018-05-29 13:20:14 -0700

    assignment testing complete

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -50,6 +50,10 @@ module.exports = class MenuController {
         return this.contacts.length;
     }
 
+    remindMe(){
+        return "Learning is a life-long pursuit";
+    }
+
     exit(){
         console.log("thanks for using AddressBloc!")
         process.exit();
